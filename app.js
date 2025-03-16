7bc36d571d020db0bde2573861c4e98543dfc047
lanceze <diegovsm88@gmail.com>
2025-03-16 19:29:53 -0300

    actualiza código app.js
    -se añaden validaciones para botón de agregar y de sorteo
    -se limpia campo de entrada después de agregar amigo

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const listaAmigos = [];
 const ulListaAmigos = document.getElementById("listaAmigos");
 const ulResultado = document.getElementById("resultado");
 
+console.log(document.getElementById("amigo").value);
+
 function agregarAmigo() {
-    listaAmigos.push(inputAmigo.value);
-    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
-    
+    if (document.getElementById("amigo").value == "") {
+        alert("Por favor, escriba un nombre")
+    } else {
+        listaAmigos.push(inputAmigo.value);
+        ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+        document.getElementById("amigo").value = null;
+    }
 }
 
-function sortearAmigo() {
+function sortearAmigo(){
+    if (listaAmigos.length > 0) {
     const random = Math.floor(Math.random() * listaAmigos.length);
     const amigoSecreto = listaAmigos[random];
-    ulResultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`;
+    ulResultado.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}</li>`
+    }  else {
+        alert("Debe ingresar amigos a la lista antes de sortear");
+    };
 }
